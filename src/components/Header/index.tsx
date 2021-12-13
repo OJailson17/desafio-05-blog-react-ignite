3bf7184bfd38094fe40b3f27c5e62a7c6c81d856
OJailson17 <JaylsonO17@gmail.com>
2021-12-13 15:21:40 -0300

    header added globally

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.container}>
+      <nav>
+        <Image src="/assets/logo.svg" width="239" height="27" priority />
+      </nav>
+    </header>
+  );
 }
