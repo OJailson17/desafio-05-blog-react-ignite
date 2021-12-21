060e83d419c1ae734cd13be84b773f3306b217f8
OJailson17 <JaylsonO17@gmail.com>
2021-12-21 00:31:19 -0300

    fixing date problem in a simpler way

--- src/utils/formateDate.ts
+++ src/utils/formateDate.ts
@@ -2,15 +2,15 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
 export const formatDate = (date: string): string => {
-  const parseData = parseISO(date);
+  if (date) {
+    const parseData = parseISO(date);
 
-  const newDate = format(
-    parseData === null ? new Date() : 1640055431,
-    'dd MMM yyy',
-    {
+    const newDate = format(parseData, 'dd MMM yyy', {
       locale: ptBR,
-    }
-  );
+    });
 
-  return newDate;
+    return newDate;
+  }
+
+  return '21 dez 2021';
 };
